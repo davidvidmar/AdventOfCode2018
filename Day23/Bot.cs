6b6406b952756f2c6dcb66297db2128f491c1d8b
David Vidmar <info@vidmar.net>
2018-12-23 22:58:17 +0100

    Day 23

---
+++ Day23/Bot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day23
+{
+    class Bot
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public int Radius { get; set; }
+
+        public Bot(string s)
+        {
+            var regex = new Regex("^pos=<(.*),(.*),(.*)>, r=(.*)$");
+            var g = regex.Match(s);
+            if (g.Groups.Count == 5)
+            {
+                X = Int32.Parse(g.Groups[1].Value);
+                Y = Int32.Parse(g.Groups[2].Value);
+                Z = Int32.Parse(g.Groups[3].Value);
+                Radius = Int32.Parse(g.Groups[4].Value);
+            }
+        }
+
+        public bool InRange(Bot bot)
+        {
+            return DistanceTo(bot) <= bot.Radius;
+        }
+
+        public int DistanceTo(Bot bot)
+        {
+            return Math.Abs(bot.X - X) + Math.Abs(bot.Y - Y) + Math.Abs(bot.Z - Z);
+        }
+
+        public Bot(int x, int y, int z, int radius)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            Radius = radius;
+        }
+
+        public override string ToString()
+        {
+            return $"pos=<{X},{Y},{Z}>, r={Radius}";
+        }
+    }
+}
---
+++ Day23/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2018 - Day 23\n");
+
+            Console.WriteLine("Part 1\n");
+
+            var example = new[] { "pos=<0,0,0>, r=4",
+                                   "pos=<1,0,0>, r=1",
+                                   "pos=<4,0,0>, r=3",
+                                   "pos=<0,2,0>, r=1",
+                                   "pos=<0,5,0>, r=3",
+                                   "pos=<0,0,3>, r=1",
+                                   "pos=<1,1,1>, r=1",
+                                   "pos=<1,1,2>, r=1",
+                                   "pos=<1,3,1>, r=1" };
+
+            var bots = Parse(example);
+            var strongest = bots.OrderByDescending(b => b.Radius).First();
+            Console.WriteLine("Example: " + bots.Count(b => b.InRange(strongest)));
+
+            var input = File.ReadAllLines("input.txt");
+            bots = Parse(input);
+            strongest = bots.OrderByDescending(b => b.Radius).First();
+            Console.WriteLine("Result: " + bots.Count(b => b.InRange(strongest)));
+
+            Console.WriteLine("\nPart 2\n");
+
+            example = new[] {
+                "pos=<10,12,12>, r=2",
+                "pos=<12,14,12>, r=2",
+                "pos=<16,12,12>, r=4",
+                "pos=<14,14,14>, r=6",
+                "pos=<50,50,50>, r=200",
+                "pos=<10,10,10>, r=5"
+            };
+            //bots = Parse(example);
+            bots = Parse(input);
+
+            //var divider = 10;
+            var divider = 100000000;
+            var factor = 10;
+            int minx = 0, miny = 0, minz = 0, maxx = 0, maxy = 0, maxz = 0;
+            while (true)
+            {
+                Console.WriteLine(divider + "...");
+                var (mx, my, mz, max) = FindSpace(bots, divider, minx * factor, miny * factor, minz * factor, maxx * factor, maxy * factor, maxz * factor);
+
+                minx = mx;
+                miny = my;
+                minz = mz;
+
+                maxx = (mx + 1);
+                maxy = (my + 1);
+                maxz = (mz + 1);
+
+                if (divider > factor)
+                    divider /= factor;
+                else if (divider > 1)
+                    divider = 1;
+                else
+                    break;
+            }
+            Console.WriteLine(minx + miny + minz);
+        }
+
+        private static (int, int, int, int) FindSpace(List<Bot> bots, int divider, int minX, int minY, int minZ, int maxX, int maxY, int maxZ)
+        {
+            var botsM = bots.Select(b => new Bot(b.X / divider, b.Y / divider, b.Z / divider, b.Radius / divider));
+
+            if (minX == 0)
+                minX = botsM.Min(b => b.X);
+            if (minY == 0)
+                minY = botsM.Min(b => b.Y);
+            if (minZ == 0)
+                minZ = botsM.Min(b => b.Z);
+
+            if (maxX == 0)
+                maxX = botsM.Max(b => b.X);
+            if (maxY == 0)
+                maxY = botsM.Max(b => b.Y);
+            if (maxZ == 0)
+                maxZ = botsM.Max(b => b.Z);            
+
+            int max = 0;
+            int mx = 0, my = 0, mz = 0;
+
+            for (int x = minX; x < maxX; x++)
+                for (int y = minY; y < maxY; y++)
+                    for (int z = minZ; z < maxZ; z++)
+                    {
+                        var ir = botsM.Count(b => b.InRange(new Bot(x, y, z, b.Radius)));
+                        if (max < ir)
+                        {
+                            mx = x;
+                            my = y;
+                            mz = z;
+                            max = ir;
+                        }
+                    }
+            Console.WriteLine((mx, my, mz, max).ToString());
+            return (mx, my, mz, max);
+        }
+
+        private static List<Bot> Parse(string[] example)
+        {
+            var list = new List<Bot>();
+            foreach (var item in example)
+            {
+                list.Add(new Bot(item));
+            }
+            return list;
+        }
+    }
+}
